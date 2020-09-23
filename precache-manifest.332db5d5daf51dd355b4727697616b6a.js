08b0fbd5f4abf5698b44108f55645cefd59e1c13
V S Sreejeet <vsreejeet@gmail.com>
2020-09-23 16:25:54 +0530

    Updates

---
+++ precache-manifest.332db5d5daf51dd355b4727697616b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ffa0b63a02b20f74ba585c9afea499",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "3418f50ee1ba1fd728a3",
+    "url": "/react-test/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9eaed6ee1eb92c478b59",
+    "url": "/react-test/static/js/2.3781c69c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-test/static/js/2.3781c69c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3418f50ee1ba1fd728a3",
+    "url": "/react-test/static/js/main.7846dfc8.chunk.js"
+  },
+  {
+    "revision": "8633f245e3fd5c82f2da",
+    "url": "/react-test/static/js/runtime-main.a78351f1.js"
+  }
+]);
